a1ceea243ac904eff63dbf13c4da9fa98ce66b56
unknown <Administrator@KANINI-LTP-468.gisbizdev.gisbiz.chennai.com>
2022-08-31 14:14:04 +0530

    'commit'

---
+++ MainPizzaApplication/Controllers/UserController.cs
@@ -0,0 +1,77 @@
+﻿using MainPizzaApplication.Models;
+using MainPizzaApplication.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MainPizzaApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService service)
+        {
+            _userService = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+       /* public ActionResult Create(UserDTO userDTO)
+        {
+            try
+            {
+                UserDTO user = _userService.Register(userDTO);
+                if(user!=null)
+                {
+                    TempData["token"] = user.jwtToken;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            catch
+            {
+
+                return View();
+            }
+            ViewBag.Error = "Not Registered";
+            return View();
+        }*/
+
+        public ActionResult Login(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserDTO userDTO)
+        {
+            try
+            {
+                UserDTO user = _userService.Login(userDTO);
+                if (user != null)
+                {
+                    TempData["token"] = user.jwtToken;
+                    return RedirectToAction("GetAll", "Employee");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+            ViewBag.Error = "Invalid Username or Password";
+            return View();
+        }
+    }
+}
